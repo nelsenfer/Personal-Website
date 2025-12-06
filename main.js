7892cf41a742339504c8dc031e5e358b0f366280
nels <nelsenfer17@gmail.com>
2025-12-07 01:04:38 +0700

    new theme

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Website siap digunakan!");
-});
-
-// popup
 const modal = document.getElementById("developmentModal");
 
-// Fungsi untuk menampilkan popup
-function showModal() {
-  modal.style.display = "flex";
-}
-
-// Fungsi untuk menyembunyikan popup
 function closeModal() {
   modal.style.display = "none";
 }
 
-// Tampilkan popup saat halaman selesai dimuat (lebih baik menggunakan event listener)
-window.onload = showModal;
+window.onload = () => {
+  modal.style.display = "flex";
+};
